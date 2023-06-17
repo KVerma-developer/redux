87a1b11b750dee1045ada7c769ee7b4f2fc375e2
Kanhaiya Verma <kanhaiyaverma.developer@gmail.com>
2023-06-17 15:09:52 +0530

    take input from client and redux done

--- reduxwithreact/src/redux/store.js
+++ reduxwithreact/src/redux/store.js
@@ -1,9 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import {applyMiddleware}   from 'redux';
 import rootReducer from './rootReducer';
 import logger from 'redux-logger';
 
-const middleware = [...applyMiddleware(), logger];
+const middleware = [logger];
 
 const store = configureStore({
   reducer: rootReducer,
